4cebfd53dcfc04680bb7f58c03b4c58c263d9cad
Clement Murphy <clemmurphy@gmail.com>
2022-02-25 17:11:16 +0000

    Initial commit

---
+++ randomwalk.js
@@ -0,0 +1,54 @@
+let x
+let y
+const stepSize = 6
+
+window.addEventListener('DOMContentLoaded', () => {
+  const clearButton = document.getElementById('reset-button')
+  clearButton.addEventListener('click', () => {
+    setup()
+  })
+  const saveButton = document.getElementById('save-button')
+  saveButton.addEventListener('click', () => {
+    saveCanvas(canvas, 'generative-walk', '.jpg')
+  })
+})
+
+function setup() {
+  const canvas = createCanvas(windowWidth, windowHeight)
+  background(51)
+  x = floor(height / 2)
+  y = floor(width / 2)
+  frameRate(60)
+}
+
+function draw() {
+  stroke(random(120, 255))
+  strokeWeight(3)
+
+  const r = floor(random(4))
+
+  if (x > 0 && x <= width && y > 0 && y <= height) {
+    point(x, y)
+  } else {
+  }
+
+  switch (r) {
+    case 0:
+      x = x + stepSize
+      break
+    case 1:
+      x = x - stepSize
+      break
+    case 2:
+      y = y + stepSize
+      break
+    case 3:
+      y = y - stepSize
+      break
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
+  background(51)
+}
